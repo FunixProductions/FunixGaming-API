d2a89e9550e8d46dd710864144c1c39de8cc4821
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-03-27 20:04:36 +0300

    edit urls

--- api/src/main/java/fr/funixgaming/api/client/funixbot/clients/FunixBotCommandClient.java
+++ api/src/main/java/fr/funixgaming/api/client/funixbot/clients/FunixBotCommandClient.java
@@ -4,6 +4,6 @@ import fr.funixgaming.api.core.clients.CrudClient;
 import fr.funixgaming.api.client.funixbot.dtos.FunixBotCommandDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "FunixBotCommands", url = "${app.domain.url}/funixbot/commands")
+@FeignClient(name = "FunixBotCommands", url = "${app.domain.url}/funixbot/commands/")
 public interface FunixBotCommandClient extends CrudClient<FunixBotCommandDTO> {
 }
