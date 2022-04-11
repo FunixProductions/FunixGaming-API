a75b0d089bc8f7d9ac9d8d72003e040078cc122a
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-04-11 12:27:15 +0300

    wip mails

---
+++ service/src/main/java/fr/funixgaming/api/server/utils/mail/MailResource.java
@@ -0,0 +1,12 @@
+package fr.funixgaming.api.server.utils.mail;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("mail")
+public class MailResource {
+
+    
+
+}
