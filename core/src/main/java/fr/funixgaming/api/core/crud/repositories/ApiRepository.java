7f02b01d1e46963343301014d63889c45dd8a6c1
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-04-26 17:54:00 +0300

    add paging repo

--- core/src/main/java/fr/funixgaming/api/core/crud/repositories/ApiRepository.java
+++ core/src/main/java/fr/funixgaming/api/core/crud/repositories/ApiRepository.java
@@ -3,11 +3,12 @@ package fr.funixgaming.api.core.crud.repositories;
 import fr.funixgaming.api.core.crud.entities.ApiEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Optional;
 
 @NoRepositoryBean
-public interface ApiRepository<ENTITY extends ApiEntity> extends CrudRepository<ENTITY, Long> {
+public interface ApiRepository<ENTITY extends ApiEntity> extends PagingAndSortingRepository<ENTITY, Long> {
     Optional<ENTITY> findByUuid(String uuid);
     Iterable<ENTITY> findAllByUuid(String uuid);
 }
