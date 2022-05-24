646eac4ab0a59468bbd92ab0641076383f3f6ea5
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-05-24 13:36:08 +0300

    fixes on feign clients

--- api/src/main/java/fr/funixgaming/api/client/funixbot/clients/FunixBotCommandClient.java
+++ api/src/main/java/fr/funixgaming/api/client/funixbot/clients/FunixBotCommandClient.java
@@ -1,9 +1,15 @@
 package fr.funixgaming.api.client.funixbot.clients;
 
+import fr.funixgaming.api.client.config.FunixApiAuthConfig;
 import fr.funixgaming.api.core.crud.clients.CrudClient;
 import fr.funixgaming.api.client.funixbot.dtos.FunixBotCommandDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "FunixBotCommands", url = "${app.domain.url}", path = "/funixbot/command/")
+@FeignClient(
+        name = "FunixBotCommands",
+        url = "${app.domain.url}",
+        path = "/funixbot/command/",
+        configuration = FunixApiAuthConfig.class
+)
 public interface FunixBotCommandClient extends CrudClient<FunixBotCommandDTO> {
 }
