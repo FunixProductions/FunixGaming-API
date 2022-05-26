d786525b79debf1a74d94ce9e1c236d2e589c06e
Antoine PRONNIER (Funix) <funixgaming7@gmail.com>
2022-05-26 19:21:19 +0300

    [Docker] - Need fixing networking

--- service/docker/docker_funix_utils/config/init.sql
+++ service/docker/docker_funix_utils/config/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS funixgaming_web;
+CREATE DATABASE IF NOT EXISTS funixgaming_web_docker;
 
 CREATE USER 'funix'@'%' IDENTIFIED BY 'funix';
 GRANT ALL PRIVILEGES ON *.* TO 'funix'@'%';
