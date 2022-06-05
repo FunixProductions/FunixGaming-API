557e0d3ffd97680b9eecb6ab7de532be9bfd48bb
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-05 15:56:23 +0300

    remove useless import

--- api/src/main/java/fr/funixgaming/api/client/config/FunixApiConfig.java
+++ api/src/main/java/fr/funixgaming/api/client/config/FunixApiConfig.java
@@ -3,7 +3,6 @@ package fr.funixgaming.api.client.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
