fd73883eae60acc0e3936fa803d9e166c210d211
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-10-13 00:22:22 +0200

    More encryption security

--- core/src/main/java/fr/funixgaming/api/core/utils/encryption/Encryption.java
+++ core/src/main/java/fr/funixgaming/api/core/utils/encryption/Encryption.java
@@ -4,26 +4,31 @@ import fr.funixgaming.api.core.exceptions.ApiException;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.util.Base64;
 
 @Component
 public class Encryption {
-    private static final String CRYPT_ALGORITHM = "AES";
+    private static final String ALGORITHM_KEY = "AES";
+    private static final String CRYPT_ALGORITHM = "AES/GCM/NoPadding";
+    private static final int KEY_SIZE = 256;
+    private static final int IV_LENGTH_BYTE = 16;
+    private static final int TAG_LENGTH_BIT = 128;
 
     private final Base64.Encoder base64Encoder;
     private final Base64.Decoder base64Decoder;
     private final Key key;
+    private final byte[] iv;
 
     public Encryption() {
         this.key = getKeyFromFile();
+        this.iv = getIvFromFile();
         this.base64Encoder = Base64.getEncoder();
         this.base64Decoder = Base64.getDecoder();
     }
@@ -35,12 +40,12 @@ public class Encryption {
             }
 
             final Cipher cipher = Cipher.getInstance(CRYPT_ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
+            cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
 
             final byte[] objectBytes = object.getBytes(StandardCharsets.UTF_8);
             final byte[] encrypted = cipher.doFinal(objectBytes);
             return base64Encoder.encodeToString(encrypted);
-        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
+        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException e) {
             throw new ApiException("Une erreur est survenue lors de l'encryption.", e);
         }
     }
@@ -52,12 +57,12 @@ public class Encryption {
             }
 
             final Cipher cipher = Cipher.getInstance(CRYPT_ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key);
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(TAG_LENGTH_BIT, iv));
 
             final byte[] base64Decoded = base64Decoder.decode(dbData);
             final byte[] decrypted = cipher.doFinal(base64Decoded);
             return new String(decrypted, StandardCharsets.UTF_8);
-        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
+        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException e) {
             throw new ApiException("Une erreur est survenue lors du décryptage.", e);
         }
     }
@@ -73,8 +78,8 @@ public class Encryption {
                     throw new ApiException("Impossible de créer un nouveau fichier d'encryption.");
                 }
 
-                final KeyGenerator keyGenerator = KeyGenerator.getInstance(CRYPT_ALGORITHM);
-                keyGenerator.init(256);
+                final KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM_KEY);
+                keyGenerator.init(KEY_SIZE);
                 final Key key = keyGenerator.generateKey();
 
                 final String keyString = base64Encoder.encodeToString(key.getEncoded());
@@ -91,7 +96,41 @@ public class Encryption {
 
             final String keyString = Files.readString(keyFile.toPath(), StandardCharsets.UTF_8);
             final byte[] decodedKey = base64Decoder.decode(keyString);
-            return new SecretKeySpec(decodedKey, 0, decodedKey.length, CRYPT_ALGORITHM);
+            return new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITHM_KEY);
+        } catch (Exception e)  {
+            throw new ApiException("Une erreur est survenue lors de la création du fichier d'encryption.", e);
+        }
+    }
+
+    private static byte[] getIvFromFile() {
+        final Base64.Encoder base64Encoder = Base64.getEncoder();
+        final Base64.Decoder base64Decoder = Base64.getDecoder();
+        final File ivFile = new File("crypt.iv");
+
+        try {
+            if (!ivFile.exists()) {
+                if (!ivFile.createNewFile()) {
+                    throw new ApiException("Impossible de créer un nouveau fichier d'encryption.");
+                }
+
+                final SecureRandom secureRandom = new SecureRandom();
+                final byte[] iv = new byte[IV_LENGTH_BYTE];
+                secureRandom.nextBytes(iv);
+
+                final String ivString = base64Encoder.encodeToString(iv);
+                try {
+                    Files.writeString(ivFile.toPath(), ivString, StandardOpenOption.TRUNCATE_EXISTING);
+                } catch (Exception e) {
+                    if (!ivFile.delete()) {
+                        throw new ApiException("Impossible de supprimer le fichier d'encryption. Lors d'une erreur.", e);
+                    }
+                    throw e;
+                }
+                return iv;
+            }
+
+            final String ivString = Files.readString(ivFile.toPath(), StandardCharsets.UTF_8);
+            return base64Decoder.decode(ivString);
         } catch (Exception e)  {
             throw new ApiException("Une erreur est survenue lors de la création du fichier d'encryption.", e);
         }
