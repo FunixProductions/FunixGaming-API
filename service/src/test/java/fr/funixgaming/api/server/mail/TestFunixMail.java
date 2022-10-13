594f84a0a88a90fda38cd16be27091c77392b73c
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-10-13 22:18:31 +0200

    tryfix mails tests

--- service/src/test/java/fr/funixgaming/api/server/mail/TestFunixMail.java
+++ service/src/test/java/fr/funixgaming/api/server/mail/TestFunixMail.java
@@ -62,7 +62,7 @@ public class TestFunixMail {
         assertEquals(funixMailDTO.getFrom(), response.getFrom());
 
         Thread.sleep(1000);
-        assertTrue(greenMail.waitForIncomingEmail(15000, 2));
+        assertTrue(greenMail.waitForIncomingEmail(15000, 1));
 
         final FunixMailDTO data = getMailById(response.getId().toString());
         assertTrue(data.isSend());
