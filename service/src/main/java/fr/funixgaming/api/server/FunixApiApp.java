9a548d8c3ffc6c550177ecff4095c23ea1614952
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-12-02 16:10:19 +0100

    wip spring 3 - standby migration, too much incompatibilities

--- service/src/main/java/fr/funixgaming/api/server/FunixApiApp.java
+++ service/src/main/java/fr/funixgaming/api/server/FunixApiApp.java
@@ -1,8 +1,10 @@
 package fr.funixgaming.api.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableFeignClients(basePackages = "fr.funixgaming.api")
 @SpringBootApplication(scanBasePackages = "fr.funixgaming.api")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class FunixApiApp {
 
     public static void main(final String[] args) {
