5c1444663147573587af886427049d8f4a1c0ecc
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-12-24 13:16:22 +0100

    done little unit tests, will make more tests later (time consuming)

--- api/src/main/java/fr/funixgaming/api/client/external_api_impl/twitch/reference/clients/stream/TwitchStreamsClient.java
+++ api/src/main/java/fr/funixgaming/api/client/external_api_impl/twitch/reference/clients/stream/TwitchStreamsClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
-        name = "TwitchChatClient",
+        name = "TwitchStreamsClient",
         url = "${funix.api.app-domain-url}",
         path = "twitch/streams"
 )
