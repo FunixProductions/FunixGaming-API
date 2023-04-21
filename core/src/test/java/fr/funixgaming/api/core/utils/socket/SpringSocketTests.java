e37f290f0ce2786a51380722ff40e71b1f64aef9
FunixG <antoine.pronnier@gmail.com>
2023-04-21 17:24:30 +0200

    fix test

--- core/src/test/java/fr/funixgaming/api/core/utils/socket/SpringSocketTests.java
+++ core/src/test/java/fr/funixgaming/api/core/utils/socket/SpringSocketTests.java
@@ -3,6 +3,7 @@ package fr.funixgaming.api.core.utils.socket;
 import fr.funixgaming.api.core.exceptions.ApiException;
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,14 +16,21 @@ class SpringSocketTests {
     private final AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     @Test
-    void testServer() throws InterruptedException {
+    void testServer() {
         try {
             final String toCheck = "oui je suis un test";
             final TestServerSocket server = new TestServerSocket(port, atomicBoolean, toCheck);
             final TestClientSocket client = new TestClientSocket(ip, port, atomicBoolean, toCheck);
 
             client.sendMessage(toCheck);
-            Thread.sleep(3000);
+
+            final Instant start = Instant.now();
+            while (!atomicBoolean.get()) {
+                if (Instant.now().getEpochSecond() - start.getEpochSecond() > 15) {
+                    fail("Timeout");
+                }
+            }
+
             assertTrue(atomicBoolean.get());
             server.closeServer();
             client.close();
