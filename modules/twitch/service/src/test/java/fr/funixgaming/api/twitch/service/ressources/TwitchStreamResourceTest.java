725c57d28fb499c2895a881484f46b0598b0b277
FunixG <antoine.pronnier@gmail.com>
2023-07-17 15:57:59 +0200

    fix test

--- modules/twitch/service/src/test/java/fr/funixgaming/api/twitch/service/ressources/TwitchStreamResourceTest.java
+++ modules/twitch/service/src/test/java/fr/funixgaming/api/twitch/service/ressources/TwitchStreamResourceTest.java
@@ -2,6 +2,9 @@ package fr.funixgaming.api.twitch.service.ressources;
 
 import com.funixproductions.api.twitch.reference.client.clients.stream.TwitchStreamsClient;
 import com.funixproductions.api.twitch.reference.client.dtos.responses.TwitchDataResponseDTO;
+import com.funixproductions.api.user.client.clients.UserAuthClient;
+import com.funixproductions.api.user.client.dtos.UserDTO;
+import com.funixproductions.api.user.client.enums.UserRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Date;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -22,12 +28,24 @@ class TwitchStreamResourceTest {
     @MockBean
     private TwitchStreamsClient twitchStreamsClient;
 
+    @MockBean
+    private UserAuthClient userAuthClient;
+
     @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     void setupMocks() {
         when(twitchStreamsClient.getStreams(anyString())).thenReturn(new TwitchDataResponseDTO<>());
+
+        final UserDTO userDTO = new UserDTO();
+        userDTO.setRole(UserRole.MODERATOR);
+        userDTO.setEmail("oui@gmail.com");
+        userDTO.setId(UUID.randomUUID());
+        userDTO.setCreatedAt(new Date());
+        userDTO.setUsername("toto");
+
+        when(userAuthClient.current(anyString())).thenReturn(userDTO);
     }
 
     @Test
