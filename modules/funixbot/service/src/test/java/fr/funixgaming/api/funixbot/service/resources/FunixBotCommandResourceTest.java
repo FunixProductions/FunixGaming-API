0fa7e4632e28efcb12d4c9ec7b65b4b1a5d5436f
FunixG <antoine.pronnier@gmail.com>
2023-07-18 14:38:38 +0200

    fix build

---
+++ modules/funixbot/service/src/test/java/fr/funixgaming/api/funixbot/service/resources/FunixBotCommandResourceTest.java
@@ -0,0 +1,25 @@
+package fr.funixgaming.api.funixbot.service.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FunixBotCommandResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetCommands() throws Exception {
+        mockMvc.perform(get("/funixbot/command"))
+                .andExpect(status().isOk());
+    }
+
+}
