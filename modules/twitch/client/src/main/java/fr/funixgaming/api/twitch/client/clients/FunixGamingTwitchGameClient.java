c910c88f7ea1f4e4d540904cd9fb98ef1be442c9
FunixG <antoine.pronnier@gmail.com>
2023-07-18 17:09:18 +0200

    add conf

--- modules/twitch/client/src/main/java/fr/funixgaming/api/twitch/client/clients/FunixGamingTwitchGameClient.java
+++ modules/twitch/client/src/main/java/fr/funixgaming/api/twitch/client/clients/FunixGamingTwitchGameClient.java
@@ -2,6 +2,7 @@ package fr.funixgaming.api.twitch.client.clients;
 
 import com.funixproductions.api.twitch.reference.client.dtos.responses.TwitchDataResponseDTO;
 import com.funixproductions.api.twitch.reference.client.dtos.responses.game.TwitchGameDTO;
+import fr.funixgaming.api.core.client.clients.FeignTokenInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(
         name = "FunixGamingTwitchGameClient",
         url = "${funixgaming.api.twitch.app-domain-url}",
-        path = "/twitch/games"
+        path = "/twitch/games",
+        configuration = FeignTokenInterceptor.class
 )
 public interface FunixGamingTwitchGameClient {
 
--- modules/twitch/client/src/main/java/fr/funixgaming/api/twitch/client/clients/FunixGamingTwitchUserClient.java
+++ modules/twitch/client/src/main/java/fr/funixgaming/api/twitch/client/clients/FunixGamingTwitchUserClient.java
@@ -3,6 +3,7 @@ package fr.funixgaming.api.twitch.client.clients;
 import com.funixproductions.api.twitch.reference.client.dtos.responses.TwitchDataResponseDTO;
 import com.funixproductions.api.twitch.reference.client.dtos.responses.user.TwitchFollowDTO;
 import com.funixproductions.api.twitch.reference.client.dtos.responses.user.TwitchUserDTO;
+import fr.funixgaming.api.core.client.clients.FeignTokenInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,8 @@ import java.util.List;
 @FeignClient(
         name = "FunixGamingTwitchUserClient",
         url = "${funixgaming.api.twitch.app-domain-url}",
-        path = "/twitch/user"
+        path = "/twitch/user",
+        configuration = FeignTokenInterceptor.class
 )
 public interface FunixGamingTwitchUserClient {
 
