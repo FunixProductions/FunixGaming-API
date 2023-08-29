48ea5b790986057047cd1c5fb28ccd6abbd009b2
FunixG <antoine.pronnier@gmail.com>
2023-08-29 18:34:51 +0200

    fix migration

--- modules/funixbot/service/src/main/resources/db/migration/V2__add_username_funixbot_exp.sql
+++ modules/funixbot/service/src/main/resources/db/migration/V2__add_username_funixbot_exp.sql
@@ -1,3 +1,1 @@
-ALTER TABLE funixbot_users_experience
-    ADD COLUMN twitch_username VARCHAR(200) DEFAULT '' NOT NULL,
-    ADD CONSTRAINT uk_funixbot_users_experience_twitch_username UNIQUE (twitch_username);
+ALTER TABLE funixbot_users_experience ADD COLUMN twitch_username VARCHAR(200) DEFAULT '' NOT NULL constraint funixbot_users_experience_key_twitch_username_unique unique;
