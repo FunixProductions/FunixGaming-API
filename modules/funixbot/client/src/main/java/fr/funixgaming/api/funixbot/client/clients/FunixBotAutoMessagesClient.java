39d0b3db1e453db3e258429d6704ad239a4ff337
FunixG <antoine.pronnier@gmail.com>
2023-08-29 17:24:40 +0200

    add feign config

--- modules/funixbot/client/src/main/java/fr/funixgaming/api/funixbot/client/clients/FunixBotAutoMessagesClient.java
+++ modules/funixbot/client/src/main/java/fr/funixgaming/api/funixbot/client/clients/FunixBotAutoMessagesClient.java
@@ -1,9 +1,15 @@
 package fr.funixgaming.api.funixbot.client.clients;
 
 import com.funixproductions.core.crud.clients.CrudClient;
+import fr.funixgaming.api.core.client.clients.FeignTokenInterceptor;
 import fr.funixgaming.api.funixbot.client.dtos.FunixBotAutoMessageDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "FunixBotAutoMessagesClient", url = "${funixgaming.api.funixbot.app-domain-url}", path = "/funixbot/automessages/")
+@FeignClient(
+        name = "FunixBotAutoMessagesClient",
+        url = "${funixgaming.api.funixbot.app-domain-url}",
+        path = "/funixbot/automessages/",
+        configuration = FeignTokenInterceptor.class
+)
 public interface FunixBotAutoMessagesClient extends CrudClient<FunixBotAutoMessageDTO> {
 }
